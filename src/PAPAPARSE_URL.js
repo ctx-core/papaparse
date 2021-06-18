9c754d9d60198a17b18c52b7af39304ef34980de
Brian Takita <brian.takita@gmail.com>
2021-06-18 04:09:33 -0400

    rm src/*.js

--- src/PAPAPARSE_URL.js
+++
@@ -1,4 +0,0 @@
-import { papaparse_version_ } from './papaparse_version_';
-export const PAPAPARSE_URL = process.env.PAPAPARSE_URL
-    || `https://cdnjs.cloudflare.com/ajax/libs/PapaParse/${papaparse_version_()}/papaparse.js`;
-//# sourceMappingURL=src/PAPAPARSE_URL.js.map
--- src/index.js
+++
@@ -1,3 +0,0 @@
-export * from './papaparse_version_';
-export * from './PAPAPARSE_URL';
-//# sourceMappingURL=src/index.js.map
