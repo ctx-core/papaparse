c66df1532ad1f2e3f6b7e1aaba42c2adad307c2c
Brian Takita <brian.takita@gmail.com>
2023-12-20 04:33:20 -0500

    consolidate dependencies

--- PAPAPARSE_URL/index.js
+++ PAPAPARSE_URL/index.js
@@ -1,4 +1,4 @@
-import { import_meta_env_ } from '@ctx-core/env'
+import { import_meta_env_ } from 'ctx-core/env'
 import { papaparse_version_ } from '../papaparse_version_/index.js'
 export const PAPAPARSE_URL =
 	import_meta_env_().PAPAPARSE_URL
